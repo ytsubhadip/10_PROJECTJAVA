15e5c7190afd7d86bb243fb10871a361269ecc31
ytsubhadip <159522165+ytsubhadip@users.noreply.github.com>
2025-06-02 11:40:04 +0530

    new update

--- car showroom/src/car.java
+++ car showroom/src/car.java
@@ -6,18 +6,20 @@ public class car implements utility{
     String car_color;
     String car_fuel_type;
     int car_price;
-    String car_type;
+    String car_Number;
 // get all details in car class
 public void getDetails(){
     System.out.println("Car Name: "+ car_name);
     System.out.println("Car color: "+ car_color);
     System.out.println("car fuel type: "+ car_fuel_type);
     System.out.println("car price: "+ car_price);
-    System.out.println("car type: "+ car_type);
+    System.out.println("car Number : "+ car_Number);
 }
 // set all
 public void setDetails(){
     Scanner sca = new Scanner(System.in);
+    System.out.println("======================= *** ENTER CAR DETAILS *** =======================");
+    System.out.println();
     System.out.print("Enter car name: ");
     car_name = sca.nextLine();
     System.out.print("Enter car color: ");
@@ -26,8 +28,9 @@ public void setDetails(){
     car_name = sca.nextLine();
     System.out.print("Enter car price: ");
     car_price = sca.nextInt();
+    sca.nextLine();
     System.out.print("Enter car type: ");
-    car_type = sca.nextLine();
+    car_Number = sca.nextLine();
 
 }
 
